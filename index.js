84e769c8ce7ff98ca014023a16d623455dce50bc
amo <37780913+amoramishvili@users.noreply.github.com>
2022-07-16 22:18:45 +0400

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ export default function (options) {
     transform: (source, id) => {
       if (!filter(id)) return null;
       return optimizeSvg(source, config, id).then((result) => {
-        return `<template><svg v-bind="data.attrs" ${result.substring(5, result.length)}</template>`;
+        return `<template><svg ${result.substring(5, result.length)}</template>`;
       });
     },
   };
